f02a0f1241a7b5d316bcfb398655a76eed7d78c7
LukaKrilic <lkrilic@algebra.hr>
2026-08-24 22:37:23 +0200

    Solution skeleton i Postgress docker-compose

---
+++ MedicalApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using Npgsql;
+
+using var conn = new NpgsqlConnection("Host=localhost;Username=user;Password=password;Database=medicaldb");
+conn.Open();
+Console.WriteLine(new NpgsqlCommand("SELECT version()", conn).ExecuteScalar());
